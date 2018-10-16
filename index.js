c84866081908675c29c752aa02afc4b482fdbdf6
sinchang <sinchangwen@gmail.com>
2018-10-16 20:19:14 +0800

    Add Spotify support

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 'use strict'
 
+require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api')
-
 const token = process.env.TELEGRAM_BOT_TOKEN
-
 const bot = new TelegramBot(token, {polling: true})
 
 const movie = require('./lib/movie.js')
+const spotify = require('./lib/spotify.js')
 
 bot.onText(/\/movie (.+)/, async (msg, match) => {
   const chatId = msg.chat.id
   const resp = await movie(match[1])
+  bot.sendMessage(chatId, resp)
+})
+
+bot.onText(/\/spotify (.+)/, async (msg, match) => {
+  const chatId = msg.chat.id
+  const resp = await spotify(match[1])
   bot.sendMessage(chatId, resp)
 })
